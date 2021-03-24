230fd728cca15f342ee69293def71d4786d61126
MukeshMasilamani <mukesh02mail@gmail.com>
2021-03-24 22:42:58 +0530

    Add B

--- src/org/emp/CustomerDetails.java
+++ src/org/emp/CustomerDetails.java
@@ -3,6 +3,15 @@ package org.emp;
 public class CustomerDetails {
 private void customerId() {
 	System.out.println("Cutomer Id  : 6565");
+}
+private void cusAddress() {
+	System.out.println("Customer Addres : ThiruNagar Chennai 600083");
+}
+private void cusAadharNo() {
+	System.out.println("AadharNumber  : 6585 8787 6253 8547");
+}
+private void cusStatus() {
+	System.out.println("Status : Single");
 
 }
 }
